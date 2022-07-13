274496e892fdeb4208624cd1aaab90d6f459f814
Claire <murphyclaire91@gmail.com>
2022-07-13 19:40:53 -0400

    connected mongo and react!

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
+import axios from 'axios';
 
 export default class Login extends Component {
     constructor(props) {
@@ -27,35 +28,37 @@ export default class Login extends Component {
     }
     onSubmit(e) {
         //prevents submission of default values
+        console.log("submitted!")
         e.preventDefault();
         const user = {
             username: this.state.username,
             password: this.state.password
         }
         console.log(user);
+        axios.post('http://localhost:5000',user).then(res => console.log(res.data));
  
         }
   
     componentDidMount() {
         this.setState({
-            username: "newUser",
-            password: "newPass"
+            username: "",
+            password: ""
 
-        })
+        });
     }
     render() {
         return (
 <form onSubmit = {this.onSubmit}>
-<div class="container">
-  <div class="mb-3">
-    <label for="exampleInputusername" class="form-label">Username</label>
-    <input value={this.state.username} onChange={this.onChangeUsername} class="form-control" id="exampleInputUsername" aria-describedby="emailHelp"></input>
+<div className="container mt-4">
+  <div className="mb-3">
+    <label htmlFor="exampleInputusername" className="form-label">Username</label>
+    <input value={this.state.username} onChange={this.onChangeUsername} className="form-control" id="exampleInputUsername" aria-describedby="emailHelp"></input>
   </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input value={this.state.password} onChange={this.onChangePassword}  type="password" class="form-control" id="exampleInputPassword1"></input>
+  <div className="mb-3">
+    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+    <input value={this.state.password} onChange={this.onChangePassword}  type="password" className="form-control" id="exampleInputPassword1"></input>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" className="btn btn-primary">Submit</button>
   </div>
 </form>
 
