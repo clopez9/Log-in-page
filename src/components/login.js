c46e4497f3b22311c92a4eca8ecaff7170f79e4d
Claire <murphyclaire91@gmail.com>
2022-07-13 15:27:15 -0400

    form setup

--- src/components/login.js
+++ src/components/login.js
@@ -3,18 +3,24 @@ import {Link} from "react-router-dom";
 
 export default class Login extends Component {
     constructor(props) {
+        
         super(props);
          this.state = {
             username: "",
             password: ""
          }
+         this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+   
     }
     onChangeUsername(e) {
         this.setState({
             username: e.target.value
         });
     }
-    onChangeUsername(e) {
+    onChangePassword(e) {
         this.setState({
             password: e.target.value
         });
@@ -27,13 +33,32 @@ export default class Login extends Component {
             password: this.state.password
         }
         console.log(user);
+ 
+        }
+  
+    componentDidMount() {
+        this.setState({
+            username: "newUser",
+            password: "newPass"
+
+        })
     }
     render() {
-        console.log("working");
         return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+<form onSubmit = {this.onSubmit}>
+<div class="container">
+  <div class="mb-3">
+    <label for="exampleInputusername" class="form-label">Username</label>
+    <input value={this.state.username} onChange={this.onChangeUsername} class="form-control" id="exampleInputUsername" aria-describedby="emailHelp"></input>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input value={this.state.password} onChange={this.onChangePassword}  type="password" class="form-control" id="exampleInputPassword1"></input>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+  </div>
+</form>
+
 
         );
     }
